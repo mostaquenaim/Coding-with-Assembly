28ad7ae719c6924d8a04f6cd27598e78227aef60
mostaquenaim <75275684+mostaquenaim@users.noreply.github.com>
2022-12-05 15:36:12 +0600

    Add files via upload

---
+++ Final_task7.asm
@@ -0,0 +1,52 @@
+.MODEL SMALL
+.STACK 100H
+.DATA 
+
+MSG1 DB "Welcome User!! $"
+MSG2 DB 0DH,0AH,"Please User Enter small Letter:", 0DH,0AH, "$"
+MSG3 DB 0DH,0AH,"Capital Letter :", 0DH,0AH, "$" 
+
+CRTR1 DB ?
+CRTR2 DB ?
+CRTR3 DB ?  
+
+.CODE 
+
+MAIN PROC
+     MOV AX, @DATA
+     MOV DS, AX
+     
+     LEA DX, MSG1
+     MOV AH, 09H
+     INT 21H 
+     
+     LEA DX, MSG2
+     MOV AH, 09H
+     INT 21H 
+     
+     MOV AH, 1
+     INT 21H 
+     MOV BL,AL
+     
+     CALL TASK 
+                     
+     MOV AH, 4CH
+     INT 21H
+     MAIN ENDP    
+
+TASK PROC
+     LEA DX, MSG3
+     MOV AH, 09H
+     INT 21H
+     
+     SUB BL,20H
+        
+     MOV AH,2
+     MOV DL,BL
+     INT 21H     
+     
+     
+     TASK ENDP
+     
+
+END MAIN
